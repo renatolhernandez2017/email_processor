d8065277d71ce51fee26b20c884c3c66cf31f314
Renato Hernandez <renatolhernandez@hotmail.com>
2025-03-11 19:42:58 +0000

    Criar logica para endereço completo e endereço, que esta ligado no representante

--- app/models/prescriber.rb
+++ app/models/prescriber.rb
@@ -4,4 +4,14 @@ class Prescriber < ApplicationRecord
   include PgSearch::Model
 
   belongs_to :representative, optional: true
+
+  def address
+    representative&.address
+  end
+
+  def full_address
+    return "Sem endereço" unless representative&.address
+
+    "#{representative.address.street} - #{representative.address.district} - #{representative.address.city} - #{representative.address.uf} - #{representative.address.zip_code}"
+  end
 end
