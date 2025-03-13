f00b2f659732bb616282692f55b34e612cd9543a
Renato Hernandez <renatolhernandez@hotmail.com>
2025-03-13 12:44:22 +0000

    Ajustar componente select

--- app/components/select_field_component.rb
+++ app/components/select_field_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class SelectFieldComponent < ViewComponent::Base
-  def initialize(form:, field_name:, options: {}, label: nil, error_message: nil)
+  def initialize(form:, field_name:, options: {}, label: nil, data: nil, error_message: nil)
     @form = form
     @field_name = field_name
     @options = options
     @label = label || @form.object.class.human_attribute_name(@field_name)
+    @data = data
     @error_message = error_message
     @selected = @form.object.send(@field_name)
   end
