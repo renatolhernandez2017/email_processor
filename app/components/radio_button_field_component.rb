0dbbfabff87b68fce33b3c4b95feb8609962243c
Renato Hernandez <renatolhernandez@hotmail.com>
2025-03-17 19:37:55 +0000

    Ajustar componente radio button

--- app/components/radio_button_field_component.rb
+++ app/components/radio_button_field_component.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class RadioButtonFieldComponent < ViewComponent::Base
-
+  def initialize(form:, field_name:, label: nil, error_message: nil)
+    @form = form
+    @field_name = field_name
+    @label = label
+    @error_message = error_message
+  end
 end
