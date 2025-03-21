a50b11774caed6243ba9f2ba98782aaab5b755e0
Renato Hernandez <renatolhernandez@hotmail.com>
2025-03-21 15:02:17 +0000

    Colocar selected e disabled no componente

--- app/components/select_field_component.rb
+++ app/components/select_field_component.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class SelectFieldComponent < ViewComponent::Base
-  def initialize(form:, field_name:, options: {}, label: nil, data: nil, error_message: nil)
+  def initialize(form:, field_name:, options: {}, label: nil, data: nil, selected: nil, disabled: nil, error_message: nil)
     @form = form
     @field_name = field_name
     @options = options
     @label = label || @form.object.class.human_attribute_name(@field_name)
     @data = data
     @error_message = error_message
-    @selected = @form.object.send(@field_name)
+    @selected = selected.present? ? selected : @form.object.send(@field_name)
+    @disabled = disabled
   end
 end
