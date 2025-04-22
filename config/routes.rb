e3c250e7b0066fdb2037818189f36b759c86dfa4
Renato Hernandez <renatolhernandez@hotmail.com>
2025-04-22 14:37:59 +0000

    Mudar requisições do menu para a listagem de prescritores e criar rota

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   resources :prescribers, only: %i[index create update show destroy] do
     post :change_accumulated, on: :member
     get :patient_listing, on: :member
+    get :requests, on: :member
   end
 
   resources :representatives, only: %i[index create update] do
