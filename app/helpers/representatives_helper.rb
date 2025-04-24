aa99a45b7c7fdf22f836fb64d63c6d4a36617dd8
Renato Hernandez <renatolhernandez@hotmail.com>
2025-04-24 12:51:11 +0000

    ajustar metodo do resumido mensal

--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -1,16 +1,17 @@
 module RepresentativesHelper
   include Roundable
 
-  def set_calculate_totals(monthly_reports)
-    total_by_bank(monthly_reports)
-    total_by_store(monthly_reports)
-    total_cash(monthly_reports)
+  def set_calculate_summary(monthly_reports)
+    @monthly_reports = monthly_reports
+    total_by_bank
+    total_by_store
+    total_cash
   end
 
   private
 
-  def total_by_bank(monthly_reports)
-    @totals_by_bank = monthly_reports
+  def total_by_bank
+    @totals_by_bank = @monthly_reports
       .group_by { |m| m.prescriber&.current_accounts&.find_by(standard: true)&.bank&.name }
       .reject { |bank, _| bank.nil? }
       .map do |bank, reports|
@@ -25,8 +26,8 @@ module RepresentativesHelper
     @total_bank = @totals_by_bank.sum { |bank| bank[:total] }
   end
 
-  def total_by_store(monthly_reports)
-    @totals_by_store = monthly_reports
+  def total_by_store
+    @totals_by_store = @monthly_reports
       .group_by { |m| m.prescriber&.representative&.branch&.name }
       .map do |branch, reports|
       {
@@ -40,8 +41,8 @@ module RepresentativesHelper
     @total_store = @totals_by_store.sum { |store| store[:total] }
   end
 
-  def total_cash(monthly_reports)
-    @total_in_cash = monthly_reports
+  def total_cash
+    @total_in_cash = @monthly_reports
       .joins(prescriber: :current_accounts)
       .where.not(prescriber: {current_accounts: {id: nil}})
       .map { |mr| divide_into_notes(mr.available_value.to_f) }
