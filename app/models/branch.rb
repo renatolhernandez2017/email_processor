4bab633543c94e7f194307bebc1cca5033e2e5c1
Renato Hernandez <renatolhernandez@hotmail.com>
2025-05-26 13:14:42 +0000

    criar a busca na filial

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -6,4 +6,16 @@ class Branch < ApplicationRecord
   has_many :current_accounts, dependent: :destroy
   has_many :representatives, dependent: :destroy
   has_many :requests, dependent: :destroy
+
+  pg_search_scope :search_global,
+    against: [:name],
+    using: {
+      tsearch: {
+        prefix: true,
+        any_word: true, # Busca qualquer palavra do nome
+        dictionary: "portuguese"
+      }
+    },
+    order_within_rank: "name",
+    ignoring: :accents
 end
