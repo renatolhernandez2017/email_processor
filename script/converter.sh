caf0dc5583f73586415b46838e9777108923e4eb
Renato Hernandez <renatolhernandez@hotmail.com>
2025-06-27 20:53:24 +0000

    ajustar o arquivo converter

--- script/converter.sh
+++ script/converter.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 NOW=$(date +"%d-%m-%y")
 WORK_DIR="/workspaces/unipharmus_v2"
 LOG_FILE="$WORK_DIR/tmp/log-$NOW.log"
@@ -21,21 +23,21 @@ echo "Agora conecta ao banco via Firebird"
 echo "Agora conecta ao banco via Firebird" >> $LOG_FILE
 echo "**** $(date) **** CRIANDO ARQUIVOS CSV **** " >> $LOG_FILE
 
-# echo "Criando arquivo das Filial" >> $LOG_FILE
-# isql-fb "192.168.0.12:D:\\Fcerta-teste\\DB\\ALTERDB.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc01000.sql" > "$TMP_DIR/fc01000.csv" 2>> $LOG_FILE
-# sleep 1
+echo "Criando arquivo das Filial"
+echo "Criando arquivo das Filial" >> $LOG_FILE
+isql-fb "192.168.0.12:D:\\Fcerta-teste\\DB\\ALTERDB.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc01000.sql" > "$TMP_DIR/fc01000.csv" 2>> $LOG_FILE
+sleep 2
 
-# echo "Criando arquivo dos Representante" >> $LOG_FILE
-# isql-fb "192.168.0.12:D:\\Fcerta-teste\\DB\\ALTERDB.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc08000.sql" > "$TMP_DIR/fc08000.csv" 2>> $LOG_FILE
-# sleep 1
+echo "Criando arquivo dos Representante"
+echo "Criando arquivo dos Representante" >> $LOG_FILE
+isql-fb "192.168.0.12:D:\\Fcerta-teste\\DB\\ALTERDB.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc08000.sql" > "$TMP_DIR/fc08000.csv" 2>> $LOG_FILE
+sleep 2
 
+echo "Criando arquivo Prescritores e Requisições"
 echo "Criando arquivo Prescritores e Requisições" >> $LOG_FILE
 envsubst < "$WORK_DIR/script/all.sql" | \
 isql-fb "192.168.0.12:D:\\Fcerta-teste\\DB\\ALTERDB.ib" -u sysdba -p masterkey > "$TMP_DIR/all.csv" 2>> $LOG_FILE
-sleep 1
-
-# echo "**** $(date) **** CONVERTENDO DADOS PARA FECHAMENTO **** " >> $LOG_FILE
-# ruby "$WORK_DIR/script/runner" -e production "Fechamento.deste_mes.efetua_fechamento" 2>> $LOG_FILE
+sleep 2
 
 echo "Fecha a conexão com a VPN"
 echo "Fecha a conexão com a VPN" >> $LOG_FILE
@@ -43,3 +45,5 @@ echo "Fecha a conexão com a VPN" >> $LOG_FILE
 
 echo "ACABOU"
 echo "**** $(date) **** ACABOU **** " >> $LOG_FILE
+
+exit 0
