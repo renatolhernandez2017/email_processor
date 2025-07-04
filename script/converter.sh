1663aeb702d6d015cbc366bcfc54220e0539c321
Renato Hernandez <renatolhernandez@hotmail.com>
2025-07-04 19:53:51 +0000

    tentativa 8

--- script/converter.sh
+++ script/converter.sh
@@ -17,14 +17,14 @@ echo "Data Inicio: $START_DATE"
 echo "Data Final: $END_DATE"
 
 echo "Criando arquivo das Filiais"
-isql-fb "192.168.0.11:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc01000.sql" > "$TMP_DIR/fc01000.csv"
+isql-fb "192.168.0.10:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc01000.sql" > "$TMP_DIR/fc01000.csv"
 
 echo "Criando arquivo dos Representantes"
-isql-fb "192.168.0.11:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc08000.sql" > "$TMP_DIR/fc08000.csv"
+isql-fb "192.168.0.10:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey < "$WORK_DIR/script/fc08000.sql" > "$TMP_DIR/fc08000.csv"
 
 echo "Criando arquivo Prescritores e Requisições"
 envsubst < "$WORK_DIR/script/all.sql" | \
-isql-fb "192.168.0.11:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey > "$TMP_DIR/all.csv"
+isql-fb "192.168.0.10:d:\\fcerta\\db\\alterdb.ib" -u sysdba -p masterkey > "$TMP_DIR/all.csv"
 
 echo "ACABOU"
 
