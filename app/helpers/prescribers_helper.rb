09d0b566e77e65a46b103df60f1f0932b43ad8e0
Renato <renatolhernandez@gmail.com>
2025-08-18 17:35:51 +0000

    Saber se esta accumulado ou desacumulado

--- app/helpers/prescribers_helper.rb
+++ app/helpers/prescribers_helper.rb
@@ -1,2 +1,8 @@
 module PrescribersHelper
+  def accumulated(prescriber)
+    monthly_report = prescriber.monthly_reports.where(accumulated: true, closing_id: @current_closing.id).last
+
+    return "<span class='text-red-500'>A</span>".html_safe if monthly_report.present?
+    "D"
+  end
 end
